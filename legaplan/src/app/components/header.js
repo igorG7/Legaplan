c603d283938c1664c145847afc73f5a9781fef39
igorG7 <giovane167@outlook.com>
2024-09-23 09:42:18 -0300

    Responsividade Header

--- legaplan/src/app/components/header.js
+++ legaplan/src/app/components/header.js
@@ -1,4 +1,5 @@
 import styles from "../styles/header.module.scss";
+import logo from "../public/logo.svg";
 import Image from "next/image";
 
 export default function Header() {
@@ -6,8 +7,8 @@ export default function Header() {
     <>
       <header className={styles.header}>
         <div className={styles.container}>
-          <Image src="./public/logo.svg" width={150} height={35} alt="Logo" />
-          <p className={styles.wellcome}>Bem vindo de volta, Marcus</p>
+          <Image src={logo} width={150} height={35} alt="Logo" />
+          <p className={styles.wellcome}>Bem-vindo de volta, Marcus</p>
           <p>Segunda, 01 de Setembro, de 2025</p>
         </div>
       </header>
